74f49f76178fd65dbc3872ccc201545860ba2e38
syhuyagl <ho_sy_huy@allgrow-labo.jp>
2023-02-08 14:28:22 +0700

    sp

--- src/_public/assets/js/main.js
+++ src/_public/assets/js/main.js
@@ -1,17 +1,35 @@
 $(document).ready(function () {
+  function disableScroll() {
+    var ycoord = $(window).scrollTop();
+    $(".c-header__overlay").data("ycoord", ycoord);
+    ycoord = ycoord * -1;
+    $("body")
+      .css("position", "fixed")
+      .css("left", "0px")
+      .css("right", "0px")
+      .css("top", ycoord + "px");
+  }
+  function enableScroll() {
+    $("body")
+      .css("position", "")
+      .css("left", "auto")
+      .css("right", "auto")
+      .css("top", "auto");
+    $(window).scrollTop($(".c-header__overlay").data("ycoord"));
+  }
   $(".c-gnav__item").click(function () {
-    var href = $(this).attr("id");
+    var href = $(this).attr("data-link");
     if ($(window).width() < 1024 && $(window).width() > 767) {
       $("html, body").animate(
         {
-          scrollTop: $(href).offset().top - 95,
+          scrollTop: $(href).offset().top,
         },
         "300"
       );
     } else if ($(window).width() < 768) {
       $("html, body").animate(
         {
-          scrollTop: $(href).offset().top - 57,
+          scrollTop: $(href).offset().top,
         },
         "300"
       );
@@ -23,6 +41,7 @@ $(document).ready(function () {
         "300"
       );
     }
+    $(".c-header__spnav").removeClass("c-header__spnav--active");
   });
   $(".c-top").click(function () {
     $("html, body").animate(
@@ -31,5 +50,15 @@ $(document).ready(function () {
       },
       "300"
     );
+  });
+  $(".c-header__menu").click(function () {
+    if ($(".c-header__spnav").hasClass("c-header__spnav--active")) {
+      $(".c-header__spnav").removeClass("c-header__spnav--active");
+      enableScroll();
+    } else {
+      $(".c-header__spnav").addClass("c-header__spnav--active");
+
+      disableScroll();
+    }
   });
 });
