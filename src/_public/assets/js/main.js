2291b416c1856d14f02bab7716ef85ee555c4a4e
syhuyagl <ho_sy_huy@allgrow-labo.jp>
2023-02-08 11:41:34 +0700

    pc

--- src/_public/assets/js/main.js
+++ src/_public/assets/js/main.js
@@ -1,1 +1,35 @@
-console.log("main.js");
+$(document).ready(function () {
+  $(".c-gnav__item").click(function () {
+    var href = $(this).attr("id");
+    if ($(window).width() < 1024 && $(window).width() > 767) {
+      $("html, body").animate(
+        {
+          scrollTop: $(href).offset().top - 95,
+        },
+        "300"
+      );
+    } else if ($(window).width() < 768) {
+      $("html, body").animate(
+        {
+          scrollTop: $(href).offset().top - 57,
+        },
+        "300"
+      );
+    } else {
+      $("html, body").animate(
+        {
+          scrollTop: $(href).offset().top,
+        },
+        "300"
+      );
+    }
+  });
+  $(".c-top").click(function () {
+    $("html, body").animate(
+      {
+        scrollTop: 0,
+      },
+      "300"
+    );
+  });
+});
